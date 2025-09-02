5c9709f9321cf9a2043dc4b56c17c16a0f5a26f3
Piyush <piyxsh@gmail.com>
2025-09-03 03:18:18 +1000

    init base of page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import styles from "./Projects.module.css"
+import Link from "next/link";
+
+export default function ProjectsPage() {
+	return(
+		<>
+		<div className={styles.content}>
+			<header className={styles.navbar}>
+        <div className={styles.navContainer}>
+          <div className={styles.navCentre}>
+            <Link href="/home" className={styles.navLink}>🏠 Home</Link>
+            <span className={styles.divider}> | </span>
+            <Link href="/about" className={styles.navLink}>About 💭</Link>
+            <Link href="/projects" className={styles.navLink}>Projects 💡</Link>
+            <Link href="/contact" className={styles.navLink}>Contact 📥</Link>
+          </div>
+        </div>
+      </header>
+			hi
+		</div>
+		</>
+	);
+}
