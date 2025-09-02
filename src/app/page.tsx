6e4f0f6397efade341dad6668ae450c75ed78286
Piyush <piyxsh@gmail.com>
2025-09-02 16:53:01 +1000

    fix: positioning of container and background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,10 @@ import Link from "next/link";
 export default function LandingPage() {
   return (
     <>
-      <Link href="/home" className={styles.div}>Enter My Dreams</Link>
+      {/* <Link href="/home" className={styles.div}>Enter My Dreams</Link> */}
+      <Link href="/home" style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)",}}>
+        Enter My Dreams
+      </Link>
     </>
   );
 }
