5843e83df5761e749cc1f085b6873b74e3d4cbf9
Piyush <piyxsh@gmail.com>
2025-09-02 15:30:29 +1000

    create landing page to navigate into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 'use client'
 
-import styles from "./Home.module.css"
+import styles from "./Landing.module.css"
 import Link from "next/link";
 
 export default function LandingPage() {
   return (
     <>
-    <div className={styles.container}>
       <Link href="/home" className={styles.div}>Enter My Dreams</Link>
-    </div>
     </>
   );
 }
