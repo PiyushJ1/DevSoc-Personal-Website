6373adf18c3bc076e82787ef37a7b9782a1b978a
Piyush <piyxsh@gmail.com>
2025-09-03 22:26:39 +1000

    replace quotation with &apos;

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
                 <div className={styles.dreamEmojiContainer}>
                   <span className={styles.dreamEmoji}>✨</span>
                   <span className={styles.dreamText}>
-                    Being a trainee at DevSoc's 25T3 Training Program ;{")"}
+                    Being a trainee at DevSoc&apos;s 25T3 Training Program ;{")"}
                   </span>
                 </div>
               </div>
